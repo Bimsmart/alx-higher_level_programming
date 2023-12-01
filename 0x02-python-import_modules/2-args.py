032bb26bcb9d647caeb652ec5f31817803444e8e
Bimsmart <bolaewenla66@gmail.com>
2023-12-01 08:05:20 -0800

    prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("{} argument".format(s_len - 1))
     elif s_len == 2:
         print("{} argument:".format(s_len - 1)) 
-    else:
-        print("{} arguments:".format(s_len -1))
+    else: 
+        print("{} arguments:".format(s_len - 1 ))
     for x in range(1, s_len):
-        print("{}: {}".format(x, sys.argv[x]))
+        print("{d:}: {}".format(x, sys.argv[x]))
