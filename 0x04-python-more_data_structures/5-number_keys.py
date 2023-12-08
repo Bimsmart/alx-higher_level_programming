c1eeb3b79641e6fe2355227938c6d968ac5100b0
Bimsmart <bolaewenla66@gmail.com>
2023-12-08 02:27:56 -0800

    returns the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyrthon3
+def number_keys(a_dictionary):
+    integer = 0
+    list_keys = list(a_dictionary.keys())
+    for i in list_keys:
+        integer += 1
+
+    return (integer)
