32903a22e70a88a40236e5f9d7d186dd0b36acc7
Bimsmart <bolaewenla66@gmail.com>
2023-12-10 05:29:36 -0800

    data structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def new_in_list(my_list, idx, element):
     if my_list:
         for elements in my_list[::-1]:
-            print("{:d}".format(elments))
+            print("{:d}".format(element))
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for elements in my_string:
         if elements != "c" and elements != "C":
             new_str += elements
-        return new_strr
+        return new_str
