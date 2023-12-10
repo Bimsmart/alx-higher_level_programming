9cb969b45b89bfb22291407de79049cab16eb8bd
Bimsmart <bolaewenla66@gmail.com>
2023-12-10 05:23:43 -0800

    data structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list:
-        for elm in my_list[::-1]:
-            print("{:d}".format(elm))
+        for elements in my_list[::-1]:
+            print("{:d}".format(elments))
